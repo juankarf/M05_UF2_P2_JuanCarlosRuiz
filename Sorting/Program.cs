29a9f855c971ab317d2d30f23eaefaaf94265f42
juankarf <jcrfaudiovisual@hotmail.com>
2023-03-28 14:28:41 +0200

    Sorting Resuelto

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -25,6 +25,18 @@ namespace Sorting
 
         static public List<int> Sort(List<int> list)
         {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count - i - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
             return list;
         }
     }
