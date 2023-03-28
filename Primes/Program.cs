9a500cd5b3cbdade24e4be66a522fb8569c3f928
juankarf <jcrfaudiovisual@hotmail.com>
2023-03-28 12:33:28 +0200

    Primes Resuelto

--- Primes/Program.cs
+++ Primes/Program.cs
@@ -22,15 +22,14 @@ namespace Primes
         }
         public static bool IsPrime(float number)
         {
-            if(number <= 1)
-                return false;
-            if (number == 2)
-                return true;
-            if (number % 2 == 0)
-                return false;
-
-            //TODO
-            return false;
+            for (int i = 2; i < number; i++)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
